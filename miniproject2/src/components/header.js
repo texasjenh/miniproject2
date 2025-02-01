bcb533c30077ccbb50393c5bd84b72b220e0b98c
texasjenh <texasjenh@aol.com>
2025-02-01 13:39:32 -0600

    Second commit

---
+++ miniproject2/src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>~The Book Nook~</h1>
+            <img src="./images/bookshelf.jpg" alt="colorful bookshelf" />
+        </header>
+        
+    
+    )
+}
+
+export default Header
